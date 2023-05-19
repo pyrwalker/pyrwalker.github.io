ec55326c22833b6c54dfe710d6ebc0b08edfd7b4
pyrwalker <949052790@qq.com>
2023-05-19 17:16:10 +0800

    Site updated: 2023-05-19 17:16:09

--- js/main.js
+++ js/main.js
@@ -1,72 +1,13 @@
-/* global function */
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  Global.themeInfo = {
-    theme: `Redefine v${Global.theme_config.version}`,
-    author: 'EvanNotFound',
-    repository: 'https://github.com/EvanNotFound/hexo-theme-redefine'
-  }
-
-  Global.localStorageKey = 'Global-THEME-STATUS';
-
-  Global.styleStatus = {
-    isExpandPageWidth: false,
-    isDark: false,
-    fontSizeLevel: 0,
-    isOpenPageAside: true
-  }
-
-  // print theme base info
-  Global.printThemeInfo = () => {
-    console.log(`      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`);
-  }
-
-  // set styleStatus to localStorage
-  Global.setStyleStatus = () => {
-    localStorage.setItem(Global.localStorageKey, JSON.stringify(Global.styleStatus));
-  }
-
-  // get styleStatus from localStorage
-  Global.getStyleStatus = () => {
-    let temp = localStorage.getItem(Global.localStorageKey);
-    if (temp) {
-      temp = JSON.parse(temp);
-      for (let key in Global.styleStatus) {
-        Global.styleStatus[key] = temp[key];
-      }
-      return temp;
-    } else {
-      return null;
-    }
-  }
-
-  Global.refresh = () => {
-    Global.initUtils();
-    navbarShrink.init();
-    Global.initModeToggle();
-    Global.initBackToTop();
-    if (Global.theme_config.home_banner.subtitle.text.length !== 0) {
-      Global.initTyped('subtitle');
-    }
-
-    if (Global.theme_config.plugins.mermaid.enable === true) {
-      Global.initMermaid();
-    }
-
-    if (Global.theme_config.navbar.search.enable === true) {
-      Global.initLocalSearch();
-    }
-
-    if (Global.theme_config.articles.code_block.copy === true) {
-      Global.initCopyCode();
-    }
-
-    if (Global.theme_config.articles.lazyload === true) {
-      Global.initLazyLoad();
-    }
-  }
-
-  Global.printThemeInfo();
-  Global.refresh();
-});
+window.addEventListener("DOMContentLoaded",()=>{Global.themeInfo={theme:"Redefine v"+Global.theme_config.version,author:"EvanNotFound",repository:"https://github.com/EvanNotFound/hexo-theme-redefine"},Global.localStorageKey="Global-THEME-STATUS",Global.styleStatus={isExpandPageWidth:!1,isDark:!1,fontSizeLevel:0,isOpenPageAside:!0},Global.printThemeInfo=()=>{console.log(`      ______ __  __  ______  __    __  ______                       \r
+     /\\__  _/\\ \\_\\ \\/\\  ___\\/\\ "-./  \\/\\  ___\\                      \r
+     \\/_/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-./\\ \\ \\  __\\                      \r
+        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r
+         \\/_/ \\/_/\\/_/\\/_____/\\/_/  \\/_/\\/_____/                    \r
+                                                               \r
+ ______  ______  _____   ______  ______ __  __   __  ______    \r
+/\\  == \\/\\  ___\\/\\  __-./\\  ___\\/\\  ___/\\ \\/\\ "-.\\ \\/\\  ___\\   \r
+\\ \\  __<\\ \\  __\\\\ \\ \\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r
+ \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\"\\_\\ \\_____\\ \r
+  \\/_/ /_/\\/_____/\\/____/ \\/_____/\\/_/   \\/_/\\/_/ \\/_/\\/_____/\r
+                                                               \r
+  Github: https://github.com/EvanNotFound/hexo-theme-redefine`)},Global.setStyleStatus=()=>{localStorage.setItem(Global.localStorageKey,JSON.stringify(Global.styleStatus))},Global.getStyleStatus=()=>{var _=localStorage.getItem(Global.localStorageKey);if(_){for(var e in _=JSON.parse(_),Global.styleStatus)Global.styleStatus[e]=_[e];return _}return null},Global.refresh=()=>{Global.initUtils(),navbarShrink.init(),Global.initModeToggle(),Global.initBackToTop(),0!==Global.theme_config.home_banner.subtitle.text.length&&Global.initTyped("subtitle"),!0===Global.theme_config.plugins.mermaid.enable&&Global.initMermaid(),!0===Global.theme_config.navbar.search.enable&&Global.initLocalSearch(),!0===Global.theme_config.articles.code_block.copy&&Global.initCopyCode(),!0===Global.theme_config.articles.lazyload&&Global.initLazyLoad()},Global.printThemeInfo(),Global.refresh()});
